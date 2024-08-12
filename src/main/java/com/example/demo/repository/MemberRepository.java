bdbe84434d8faca282503267839ea89d7498e8e6
JerryKiki <gasi758@gmail.com>
2024-08-12 11:50:00 +0900

    회원가입 예외처리

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -9,7 +9,11 @@ import com.example.demo.vo.Member;
 @Mapper
 public interface MemberRepository {
 
-	@Select("SELECT LAST_INSERT_ID();")
+	//tip: 여기서 여러줄로 쓰는 법
+	@Select("""
+			SELECT
+			LAST_INSERT_ID();
+			""")
 	public int getLastInsertId();
 
 //	@Insert("INSERT INTO `member` SET regDate = NOW(), updateDate = NOW(), loginId = #{loginId}, loginPw = #{loginPw}, `name` = #{name}, nickname = #{nickname}, cellphoneNum = #{cellphoneNum}, email = #{email}")
