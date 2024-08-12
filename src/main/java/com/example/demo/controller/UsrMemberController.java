8187335df71a2f7a17321e3978099e57d54cc421
JerryKiki <gasi758@gmail.com>
2024-08-12 13:29:29 +0900

    Util 추가

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.service.MemberService;
+import com.example.demo.util.Ut;
 import com.example.demo.vo.Member;
 
 @Controller
@@ -17,17 +18,17 @@ public class UsrMemberController {
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
-		if(loginId == null) return "아이디를 입력하세요.";
+		if(Ut.isEmptyOrNull(loginId)) return "아이디를 입력하세요.";
 		else {
 			Member memberCheck = memberService.getMemberByLoginId(loginId);
 			if (memberCheck!=null) return "이미 사용중인 아이디입니다.";
 		}
 		
-		if(loginPw == null) return "비밀번호를 입력하세요.";
-		if(name == null) return "이름을 입력하세요.";
-		if(nickname == null) return "닉네임을 입력하세요.";
-		if(cellphoneNum == null) return "전화번호를 입력하세요.";
-		if(email == null) return "이메일을 입력하세요.";
+		if(Ut.isEmptyOrNull(loginPw)) return "비밀번호를 입력하세요.";
+		if(Ut.isEmptyOrNull(name)) return "이름을 입력하세요.";
+		if(Ut.isEmptyOrNull(nickname)) return "닉네임을 입력하세요.";
+		if(Ut.isEmptyOrNull(cellphoneNum)) return "전화번호를 입력하세요.";
+		if(Ut.isEmptyOrNull(email)) return "이메일을 입력하세요.";
 		if(!email.contains("@")) return "올바른 이메일을 입력하세요.";
 		
 		int id = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
